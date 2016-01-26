3955da1055a2193b5af80125326163b64453fd79
carrascogit <carrascogit@modeldd.org>
2016-01-26 13:21:45 +0100

    Intalled grunt karma and bower components, including dependency on prettytype

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+/* Gruntfile for prettytype-ng
+
+ */
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            scripts: {
+
+                files: [ 'src/**/*.js'],
+
+                tasks: ['concat', 'uglify'],
+
+                options: {
+
+                    spawn:false,
+                    event:['all']
+                },
+            },
+            karma: {
+                files: ['src/**/*.js', 'test/**/*.js'],
+                tasks: ['karma:unit:run'] //NOTE the :run flag
+            }
+        },
+        concat : {
+            options : {
+                separator: ';',
+                sourceMap :true
+            },
+            dist : {
+                src  : [ '<%= watch.scripts.files %>' ],
+                dest : 'build/prettytype-ng.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                sourceMap : true,
+                sourceMapIncludeSources : true
+            },
+            build: {
+                src: '<%= concat.dist.dest %>',
+                dest: 'dist/prettytype-ng.min.js'
+            }
+        },
+
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js'
+            },
+            continuous: {
+                configFile: 'karma.conf.js',
+                singleRun: true
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.loadNpmTasks('grunt-karma');
+
+    // Default task(s).
+    grunt.registerTask('default', ['watch']);
+
+};
---
+++ karma.conf.js
@@ -0,0 +1,44 @@
+module.exports = function(config){
+    config.set({
+
+        basePath : './',
+
+
+        files: [
+            'src/**/*.js',
+            'test/common-test/typesregistry-test.js',
+            'test/roots-test/overrider_type-test.js',
+            'test/identifying-test/identifier_type-test.js',
+            'test/identifying-test/recorder_type-test.js',
+            'test/common-test/common_type-test.js',
+            'test/common-test/common_type-record-test.js',
+            'test/common-test/common_type-misc-test.js',
+            'test/common-test/qngmock.js',
+            'test/common-test/prominstr_type-test.js',
+            'test/common-test/withprominstr_type-test.js',
+            'test/common-test/appbaseurlmock.js',
+            'test/common-test/apibaseurlmock.js',
+            'test/common-test/locationngmock.js',
+            'test/common-test/common_ctrl_type-test.js',
+            'test/common-test/common_svce_type-test.js',
+            'test/common-test/prominstrexception-test.js'
+        ],
+
+        autoWatch : true,
+
+        frameworks: ['jasmine'],
+
+        browsers : ['Chrome'],
+
+        port:  9876,
+        colors:  true ,
+
+        logLevel: config.LOG_INFO,
+
+        plugins : [
+            'karma-chrome-launcher',
+            'karma-jasmine'
+        ]
+
+    });
+};
