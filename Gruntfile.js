db0a981c047e7d1f79500f9a9d30020d0a8b8745
carrascogit <carrascogit@modeldd.org>
2016-01-26 18:45:33 +0100

    Built

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,6 +60,53 @@ module.exports = function(grunt) {
                 },
             },
             karma: {
+                files: [
+
+                    /* Order of files is mandatory as stated to the dependency injection in angular module definitions */
+
+                    'bower_components/angular/angular.js',
+                    'bower_components/angular-mocks/angular-mocks.js',
+
+
+                    'bower_components/prettytype/src/**/*.js',
+
+
+                    'src/utils/exceptiondetails_svce.js',
+                    'src/utils/decoratesystemprototypes_svce.js',
+
+                    'src/typesregistry_svce.js',
+
+
+                    'src/roots/roots_types.js',
+                    'src/roots/overrider_type.js',
+                    'src/roots/overrider_svce.js',
+
+
+                    'src/identifying/identifying_types.js',
+                    'src/identifying/identifier_type.js',
+                    'src/identifying/identifier_svce.js',
+
+                    'src/identifying/record_type.js',
+                    'src/identifying/recorder_type.js',
+                    'src/identifying/recorder_svce.js',
+
+
+                    'src/common/common_types.js',
+                    'src/common/prominstrexception.js',
+                    'src/common/common_type.js',
+                    'src/common/prominstr_type.js',
+                    'src/common/prominstr_svce.js',
+                    'src/common/withprominstr_type.js',
+                    'src/common/common_svce_type.js',
+                    'src/common/common_svce.js',
+                    'src/common/common_ctrl_type.js',
+                    'src/common/common_ctrl.js',
+
+
+
+
+                    'test/typesregistry-test.js'
+                ],
                 tasks: ['karma:unit:run'] //NOTE the :run flag
             }
         },
