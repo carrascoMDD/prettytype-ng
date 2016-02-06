952fd1d65eaf2b52a01dc6faab329344249cd6ab
carrascogit <carrascogit@modeldd.org>
2016-02-06 03:04:04 +0100

    Remove date from banner in minified by grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -105,7 +105,7 @@ module.exports = function(grunt) {
         },
         uglify: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - */\n',
                 sourceMap : true,
                 sourceMapIncludeSources : true
             },
